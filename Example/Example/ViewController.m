e67c6231b4ec1ad431c8561e2e1e66fc25e27527
boundsj <jesse@rebounds.net>
2013-04-12 08:50:17 -0700

    updated example app to test ver 0.3 of ddp lib

--- Example/Example/ViewController.m
+++ Example/Example/ViewController.m
@@ -3,22 +3,51 @@
 
 @interface ViewController () <ObjectiveDDPDelegate>
 @property (strong, nonatomic) ObjectiveDDP *ddp;
+@property (weak, nonatomic) IBOutlet UIButton *sendButton;
 @end
 
 @implementation ViewController
 
+static int uniqueId = 1;
+
 - (void)viewWillAppear:(BOOL)animated
 {
-    self.ddp = [[ObjectiveDDP alloc] initWithURLString:@"wss://ddptester.meteor.com/websocket"
+    self.ddp = [[ObjectiveDDP alloc] initWithURLString:@"ws://localhost:3000/websocket"
                                               delegate:self];
-    [self.ddp reconnect];
+    [self.ddp connectWebSocket];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+
+    NSString *uid = [NSString stringWithFormat:@"%d", uniqueId++];
+    [self.ddp subscribeWith:uid name:@"things" parameters:nil];
+}
+
+- (IBAction)didTouchSend:(id)sender {
+    NSString *uid = [NSString stringWithFormat:@"%d", uniqueId++];
+
+    NSDateFormatter *formatter = [NSDateFormatter new];
+    [formatter setDateFormat:@"HH:mm:ss"];
+    NSString *dateString = [formatter stringFromDate:[NSDate date]];
+
+    [self.ddp methodWith:uid
+                  method:@"/things/insert"
+              parameters:@[@{@"_id": uid, @"msg": dateString}]];
 }
 
 #pragma mark <ObjectiveDDPDelegate>
 
-- (void)didOpen
-{
-    // implement me
+- (void)didOpen {
+    NSLog(@"================> didOpen");
+    [self.ddp connectWithSession:nil version:@"pre1" support:nil];
+}
+
+- (void)didReceiveMessage:(NSDictionary *)message {
+    NSLog(@"================> didReceiveMessage: %@", message);
+}
+
+- (void)didReceiveConnectionError:(NSError *)error {
+    NSLog(@"================> didReceiveConnectionError: %@", error);
 }
 
 @end
